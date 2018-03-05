cf53655f48adf71d8dd1b1b127e9598981a0f9a0
Jannik Heuer <Jannik@Heuer.me>
2018-03-05 01:24:20 +0100

    Added FlexForm

--- fe_user_cards/Configuration/TCA/Overrides/tt_content.php
+++ fe_user_cards/Configuration/TCA/Overrides/tt_content.php
@@ -1,8 +1,17 @@
 <?php
+    defined('TYPO3_MODE') or die();
+    $_EXTKEY = $GLOBALS['_EXTKEY'] = 'fe_user_cards';
+    $extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
+    $pluginSignature = strtolower($extensionName) . '_multiplefeusercards';
+    $pluginTitle = 'Multiple fe_user cards';
+    $pluginIcon = '';
+    $pluginType = 'list_type';
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'HamburgerJungeJr.FeUserCards',
-    'MultipleFeUserCards',
-    'Multiple fe_user cards',
-    ''
-);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+        [$pluginTitle, $pluginSignature, $pluginIcon],
+        $pluginType,
+        $_EXTKEY
+    );
+
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature]='pi_flexform';
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_multiple_fe_user_cards.xml');
